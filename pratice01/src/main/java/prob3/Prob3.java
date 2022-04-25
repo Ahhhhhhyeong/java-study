2108f4edcfd5de4efeffc6c470ab18cf71d60721
Ahhhhhhyeong <dkdud081@gmail.com>
2022-04-25 16:57:28 +0900

    Add pratice01

---
+++ pratice01/src/main/java/prob3/Prob3.java
@@ -0,0 +1,37 @@
+package prob3;
+
+import java.util.Scanner;
+
+import javax.imageio.plugins.tiff.BaselineTIFFTagSet;
+
+public class Prob3 {
+	public static int oddEvenMethod(int value) {
+		if(value % 2 == 0) {
+			return 2;
+		}
+		else {
+			return 1;
+		}
+	}
+	
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		/* 코드 작성 */
+		for(int i = 0; i < 3; i++) {
+			System.out.print("숫자를 입력하세요 : ");
+			int num = scanner.nextInt();
+			int oddEven = oddEvenMethod(num);
+			int result = 0;
+			for(int j = oddEven; j <= num; j+=2) {
+				result += j;
+			}
+			System.out.printf("결과값 : %d \n", result);
+		}
+		
+		scanner.close();
+	}
+	
+	
+}
