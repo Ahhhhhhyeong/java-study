1707a5ee1e0f87824ba4d09a6a6be8c38b837ca7
Ahhhhhhyeong <dkdud081@gmail.com>
2022-04-26 17:22:27 +0900

    pratice03의 문제 4,5번 풂

--- pratice03/src/main/java/prob04/StringUtil.java
+++ pratice03/src/main/java/prob04/StringUtil.java
@@ -1,4 +1,13 @@
 package prob04;
 
 public class StringUtil {
+	public static String concatenate(String[] str) {
+		// 문자열을 결합하여 리턴하는 메소드 구현
+		StringBuilder sBuilder = new StringBuilder();
+		for(int i = 0; i < str.length; i++) {
+			sBuilder.append(str[i]);
+		}
+		String concat = sBuilder.toString();
+		return concat;
+	}
 }
--- pratice03/src/main/java/prob05/Account.java
+++ pratice03/src/main/java/prob05/Account.java
@@ -1,4 +1,43 @@
 package prob05;
 
 public class Account {
+
+	private static String Account;
+	private static int balance;
+
+	public Account(String acc) {
+		System.out.println(acc + " 계좌가 개설되었습니다.");
+		Account = acc;
+	}
+
+	public String getAccountNo() {
+		// 계좌번호 받음
+		return Account;
+	}
+
+	public void setAccoutNo(String Account) {
+		this.Account = Account;
+	}
+
+	public int getBalance() {
+		// 계좌의 잔고
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	public void save(int save) {
+		// 입금
+		System.out.println(Account + " 계좌에 " + save + "만원이 입금되었습니다.");
+		balance = balance + save;
+	}
+
+	public void deposit(int deposit) {
+		// 출금
+		System.out.println(Account + " 계좌에 " + deposit + "만원이 출금되었습니다.");
+		balance = balance - deposit;
+	}
+
 }
